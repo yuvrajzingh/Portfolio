90e1913d15d50fba34eb4b97b00c0005894719d3
Yuvraj Singh <yuvisingh1810@gmail.com>
2024-06-18 23:58:07 +0530

    Add image in projects when in small screen

--- components/Project.tsx
+++ components/Project.tsx
@@ -23,7 +23,6 @@ export default function Project({
 
   return (
     <motion.div
-      ref={ref}
       style={{
         scale: scaleProgess,
         opacity: opacityProgess,
@@ -31,7 +30,19 @@ export default function Project({
       className="group mb-3 sm:mb-8 last:mb-0"
     >
       <section className="bg-white/100 max-w-[42rem] border-2 border-black rounded-[5px] overflow-hidden cursor-pointer sm:pr-8 relative sm:h-[20rem] hover:bg-gray-100 transition sm:group-even:pl-8 dark:text-white dark:bg-white/10 dark:hover:bg-white/20">
-        <div className="pt-4 pb-7 px-5 sm:pl-10 sm:pr-2 sm:pt-10 sm:max-w-[50%] flex flex-col h-full sm:group-even:ml-[18rem] ">
+        {/* Image for mobile screens */}
+        <div className="relative w-full pb-[75%] sm:hidden">
+          <Image
+            src={imageUrl}
+            alt="Project I worked on"
+            layout="fill"
+            objectFit="cover"
+            quality={95}
+            className="absolute top-0 left-0 w-full h-full rounded-t-lg shadow-2xl"
+          />
+        </div>
+
+        <div className="pt-4 pb-7 px-5 sm:pl-10 sm:pr-2 sm:pt-10 sm:max-w-[50%] flex flex-col h-full sm:group-even:ml-[18rem]">
           <h3 className="text-2xl font-semibold">{title}</h3>
           <p className="mt-2 leading-relaxed text-gray-700 dark:text-white/70">
             {description}
@@ -48,22 +59,21 @@ export default function Project({
           </ul>
         </div>
 
+        {/* Image for small screens and above */}
         <Image
           src={imageUrl}
           alt="Project I worked on"
           quality={95}
           className="absolute hidden sm:block top-8 -right-40 w-[28.25rem] rounded-t-lg shadow-2xl
-        transition 
-        group-hover:scale-[1.04]
-        group-hover:-translate-x-3
-        group-hover:translate-y-3
-        group-hover:-rotate-2
-
-        group-even:group-hover:translate-x-3
-        group-even:group-hover:translate-y-3
-        group-even:group-hover:rotate-2
-
-        group-even:right-[initial] group-even:-left-40"
+          transition 
+          group-hover:scale-[1.04]
+          group-hover:-translate-x-3
+          group-hover:translate-y-3
+          group-hover:-rotate-2
+          group-even:group-hover:translate-x-3
+          group-even:group-hover:translate-y-3
+          group-even:group-hover:rotate-2
+          group-even:right-[initial] group-even:-left-40"
         />
       </section>
     </motion.div>
